9cac7ffcb944d417fea5c4441cece196dfa28427
Megan Talbot <meganftalbot@gmail.com>
2017-07-19 18:14:53 -0400

    Fix deletion of tags

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,4 @@
 class Tag < ApplicationRecord
-  has_many :offerings_tags
-  has_many :offerings, through: :offerings_tags, :dependent => :delete_all
+  has_many :offerings_tags, :dependent => :nullify
+  has_many :offerings, through: :offerings_tags, :dependent => :nullify
 end
