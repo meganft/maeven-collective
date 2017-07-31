13ad21ef66e1a9e573a9ecef74938b535eb0d912
Megan Talbot <meganftalbot@gmail.com>
2017-07-31 19:26:16 -0400

    Updates carousel and takes out unnecessary elements

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,6 +3,9 @@ class LandingController < ApplicationController
   def index
     @organizations = Organization.all
     @organization = Organization.new
-    @featured = Offering.first
+    featured = Offering.all
+    @featured_first = featured[0..3]
+    @featured_second = featured[4..7]
+    @featured_third = featured[8..11]
   end
 end
