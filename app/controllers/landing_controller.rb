ca7436e9e9079eaa8ed77f49c9347d5d7d3bca4f
Megan Talbot <meganftalbot@gmail.com>
2017-08-07 19:02:46 -0400

    Adds more images and updates layout of homepage

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -4,8 +4,8 @@ class LandingController < ApplicationController
     @organizations = Organization.all
     @organization = Organization.new
     featured = Offering.all
-    @featured_first = featured[0..3]
-    @featured_second = featured[4..7]
-    @featured_third = featured[8..11]
+    @featured_first = featured[0..2]
+    @featured_second = featured[3..6]
+    @featured_third = featured[7..10]
   end
 end
