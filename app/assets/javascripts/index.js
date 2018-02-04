c240c17015a53bafb9f84e39f29c226f1dc8dc6b
Megan <meganftalbot@gmail.com>
2018-02-04 09:55:33 -0500

    Some minor cleanup in search methods

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -2,9 +2,7 @@ $(document).ready(function() {
 	$('#myCarousel').carousel({
 	interval: 10000
 	})
-
     $('#myCarousel').on('slid.bs.carousel', function() {
-    	//alert("slid");
 	});
 
   $(function() {
