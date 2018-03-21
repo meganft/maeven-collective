9664f8c3f1c96db064ca94aed5d552f8e974b801
Megan <meganftalbot@gmail.com>
2018-03-21 13:17:58 -0400

    Add faker gem and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Tag.create!(name: 'Online Course')
+Tag.create!(name: 'Event')
+Tag.create!(name: 'Conference')
+Tag.create!(name: 'Social Media Marketing')
+Tag.create!(name: 'Social Media')
+
+Category.create!(name: 'Business')
+Category.create!(name: 'Technology')
+Category.create!(name: 'Marketing')
+
+8.times do
+  organization = Organization.create!(name: Faker::Company.name, website: Faker::Internet.url('example.com'),
+    instagram: Faker::Internet.domain_name,  description: Faker::Company.catch_phrase)
+  category = Category.all.sample
+  organization.categories << category
+  offering = organization.offerings.create!(name: Faker::Job.field, price: Faker::Commerce.price,
+    website: Faker::Internet.url('example.com'), description: Faker::Company.catch_phrase)
+  tag = Tag.all.sample
+  offering.tags << tag
+end
