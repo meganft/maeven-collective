83899aa7ca58395bd34ffa5243e2a959f50b6093
Megan <meganftalbot@gmail.com>
2018-10-18 14:56:00 -0400

    Annotate

--- app/models/offerings_tag.rb
+++ app/models/offerings_tag.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: offerings_tags
+#
+#  id          :integer          not null, primary key
+#  offering_id :integer
+#  tag_id      :integer
+#
+# Indexes
+#
+#  index_offerings_tags_on_offering_id  (offering_id)
+#  index_offerings_tags_on_tag_id       (tag_id)
+#
+
 class OfferingsTag < ApplicationRecord
   belongs_to :offering
   belongs_to :tag
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,18 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  first_name      :string
+#  last_name       :string
+#  email           :string
+#  password        :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  password_digest :string
+#  role            :integer          default("default")
+#
+
 class User < ApplicationRecord
   has_secure_password
 
